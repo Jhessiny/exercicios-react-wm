c69a1164a27f8281523a5e965b1f8190616e2af2
jhessiny <jhessinymattos@gmail.com>
2021-01-11 14:24:16 -0300

    pai e filho componentes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 // import BomDia from "./componentes/BomDia";
 // import { BoaTarde, BoaNoite } from "./componentes/multiplos";
 // import Multi from "./componentes/multiplos"; usar Multi.BoaTarde
-import Saudacao from "./componentes/saudacao";
+// import Saudacao from "./componentes/saudacao";
+// import Filho from "./componentes/filho";
+import Filho from "./componentes/filho";
+import Pai from "./componentes/pai";
 
 ReactDOM.render(
   <div>
-    <Saudacao />
+    <Pai nome="Paulo" sobrenome="Silva">
+      <Filho nome="Pedro" />
+      <Filho nome="Paulo" />
+      <Filho nome="Carla" />
+      {/*nome carla sobrescreve o nome do pai*/}
+      {/* <Filho {...props} nome="Carla" />; */}
+    </Pai>
   </div>,
 
   document.getElementById("root")
